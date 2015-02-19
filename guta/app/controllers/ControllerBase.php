dee10db68518b348ab8f5894778821903246243f
Anne-laure Mesure <bluewolf33@hotmail.fr>
2015-02-19 21:29:54 +0100

    Correctif: ressources manquantes dans le ControllerBase

--- guta/app/controllers/ControllerBase.php
+++ guta/app/controllers/ControllerBase.php
@@ -5,4 +5,16 @@ use Phalcon\Mvc\Controller;
 class ControllerBase extends Controller
 {
 
+    public function initialize()
+    {
+        \Phalcon\Tag::setTitle('MyDropbox');
+
+        $this->assets
+            ->addCss("css/bootstrap.min.css")
+            ->addCss("css/styles.css");
+
+        $this->assets
+            ->addJs("js/jquery-1.11.2.min.js")
+            ->addJS("js/bootstrap.min.js");
+    }
 }
