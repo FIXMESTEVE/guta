c92ca2905bceaa9b017c545fb7a1deec25580127
Mafieu <matthieu.albert0@gmail.com>
2015-03-19 12:04:45 +0100

    Bon path envoyé en post pour les share

--- guta/public/js/share.js
+++ guta/public/js/share.js
@@ -1,16 +1,25 @@
 //Envoyer en POST la liste des fichiers et l'adresse mail rentrée
 
-var share = function(){
+$("#inputEmail").keydown(function(e){
+	if(e.keyCode == 13)
+		share();
+});
+
+share = function(){
 	var files = [];
 	//On  récupère les checkbox
 	$(document).find('input').each(function(){
 		if($(this).prop('checked')){
-			files.push($(this).attr('id'));
+			str = "files/";
+			pos = $(this).attr('id').indexOf(str);
+			target = $(this).attr('id').substring(pos + str.length);
+			files.push(target);
 		}
 	});
 	//On récupère l'adresse mail
 	var email = $('#inputEmail').val();
 
 	//On envoie en POST à file/share/
-	$.post("/guta/guta/files/share", { paths: files, userMail: email });
+	$.post("/guta/guta/files/share", { paths: files, userMail: email })
+		.always($('#inputEmail').val(''));
 }
